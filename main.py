518bbb47ef26daef79f5c9ff088b4a0043018727
Sajjan <sajjan@outside.tech>
2021-09-03 19:38:59 +0545

    ongoing

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import pygame
 
-print('hello world!')
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main():
+
+    run = True
+    while run:
+       for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+               run = False
+
+    pygame.quit() 
+
+if __name__ == "__main__":
+    main()
